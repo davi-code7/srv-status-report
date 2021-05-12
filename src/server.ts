034092542f5f12927940d5d2689b32f698ac5cae
Davi Silva <davi.silva@code7.com>
2021-05-12 17:44:27 -0300

    feat(test): test
    test

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { success } from './utils/logger/logger';
 import app from './app';
 
-const port = process.env.PORT || 5005;
+const port = process.env.PORT || 5006;
 
 app.listen(port, () => {
   success(`${process.env.APP_NAME} is listening on port: ${port}`);
